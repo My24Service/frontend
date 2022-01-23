9a8d38f616137ac46deabd9eb867dc16f7592244
Richard Jansen <richard@pedroja.tech>
2022-01-23 15:08:37 +0100

    only import and use used components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import FlashMessage from '@smartweb/vue-flash-message'
 import Loading from 'vue-loading-overlay'
 import axios from 'axios'
@@ -9,6 +8,93 @@ import VueAxios from 'vue-axios'
 import Vuelidate from 'vuelidate'
 const moment = require('moment')
 import { ColorPicker, ColorPanel } from "one-colorpicker"
+
+import { BootstrapVueIcons } from 'bootstrap-vue'
+Vue.use(BootstrapVueIcons)
+
+import { BreadcrumbPlugin } from 'bootstrap-vue'
+Vue.use(BreadcrumbPlugin)
+
+import { ButtonPlugin } from 'bootstrap-vue'
+Vue.use(ButtonPlugin)
+
+import { ButtonGroupPlugin } from 'bootstrap-vue'
+Vue.use(ButtonGroupPlugin)
+
+import { ButtonToolbarPlugin } from 'bootstrap-vue'
+Vue.use(ButtonToolbarPlugin)
+
+import { CarouselPlugin } from 'bootstrap-vue'
+Vue.use(CarouselPlugin)
+
+import { DropdownPlugin } from 'bootstrap-vue'
+Vue.use(DropdownPlugin)
+
+import { FormPlugin } from 'bootstrap-vue'
+Vue.use(FormPlugin)
+
+import { FormCheckboxPlugin } from 'bootstrap-vue'
+Vue.use(FormCheckboxPlugin)
+
+import { FormDatepickerPlugin } from 'bootstrap-vue'
+Vue.use(FormDatepickerPlugin)
+
+import { FormFilePlugin } from 'bootstrap-vue'
+Vue.use(FormFilePlugin)
+
+import { FormGroupPlugin } from 'bootstrap-vue'
+Vue.use(FormGroupPlugin)
+
+import { FormInputPlugin } from 'bootstrap-vue'
+Vue.use(FormInputPlugin)
+
+import { FormRadioPlugin } from 'bootstrap-vue'
+Vue.use(FormRadioPlugin)
+
+import { FormSelectPlugin } from 'bootstrap-vue'
+Vue.use(FormSelectPlugin)
+
+import { FormTextareaPlugin } from 'bootstrap-vue'
+Vue.use(FormTextareaPlugin)
+
+import { FormTimepickerPlugin } from 'bootstrap-vue'
+Vue.use(FormTimepickerPlugin)
+
+import { LayoutPlugin } from 'bootstrap-vue'
+Vue.use(LayoutPlugin)
+
+import { LinkPlugin } from 'bootstrap-vue'
+Vue.use(LinkPlugin)
+
+import { ModalPlugin } from 'bootstrap-vue'
+Vue.use(ModalPlugin)
+
+import { NavPlugin } from 'bootstrap-vue'
+Vue.use(NavPlugin)
+
+import { NavbarPlugin } from 'bootstrap-vue'
+Vue.use(NavbarPlugin)
+
+import { OverlayPlugin } from 'bootstrap-vue'
+Vue.use(OverlayPlugin)
+
+import { PaginationPlugin } from 'bootstrap-vue'
+Vue.use(PaginationPlugin)
+
+import { ProgressPlugin } from 'bootstrap-vue'
+Vue.use(ProgressPlugin)
+
+import { SpinnerPlugin } from 'bootstrap-vue'
+Vue.use(SpinnerPlugin)
+
+import { TablePlugin, TableLitePlugin, TableSimplePlugin } from 'bootstrap-vue'
+Vue.use(TablePlugin)
+Vue.use(TableLitePlugin)
+Vue.use(TableSimplePlugin)
+
+import { TabsPlugin } from 'bootstrap-vue'
+Vue.use(TabsPlugin)
+
 
 import App from './App.vue'
 import store from './store'
@@ -32,11 +118,10 @@ Vue.prototype.$trans = (text) => {
   return gettext(text)
 }
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
+
 Vue.use(Vuex)
 Vue.use(VueRouter)
-Vue.use(FlashMessage, {time: 1500, strategy: 'multiple'})
+Vue.use(FlashMessage, {time: 2500, strategy: 'multiple'})
 Vue.use(Loading)
 Vue.use(VueAxios, axios)
 Vue.use(Vuelidate)
