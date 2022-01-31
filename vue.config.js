20e1c5e52e2d9f87c562c9bf206d8b41a15891b5
Richard Jansen <richard@pedroja.tech>
2022-01-31 08:21:21 +0100

    build tweaks

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,8 @@
-const BundleTracker = require("webpack-bundle-tracker");
-
 module.exports = {
     filenameHashing: false,
     productionSourceMap: false,
     publicPath: '/',
     outputDir: './dist',
-    devServer: {
-      host: 'stormy.my24service.com'
-    },
-    pages: {
-      index: {
-        entry: 'src/main.js',
-        title: 'My24Service',
-      },
-    },
     chainWebpack: config => {
         config.optimization
             .splitChunks({
