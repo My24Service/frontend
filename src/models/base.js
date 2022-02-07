e482929e13cac73d332704cdf652bf7d49dd8129
Richard Jansen <richard@pedroja.tech>
2022-02-07 11:09:41 +0100

    CSRF token in header from cookie

--- src/models/base.js
+++ src/models/base.js
@@ -56,7 +56,24 @@ class BaseModel {
     this.listArgs = listArgs
   }
 
-  getHeaders(token) {
+  getTokenCookie() {
+    let cookieValue = null
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';')
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim()
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+                break
+            }
+        }
+    }
+    return cookieValue
+  }
+
+  getHeaders() {
+    const cookie = this.getTokenCookie('csrftoken')
     return {
       headers: {
         'X-CSRFToken': token,
