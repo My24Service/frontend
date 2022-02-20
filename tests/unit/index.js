67f1e58879be96253018b38fcc6e87ba9732a5c3
Richard Jansen <richard@pedroja.tech>
2022-02-20 16:14:50 +0100

    removed vue-flash-message from test index

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -3,7 +3,6 @@ import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import VueRouter from 'vue-router'
 import VueI18n from 'vue-i18n'
 import Vuex from 'vuex'
-import FlashMessage from '@smartweb/vue-flash-message';
 
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
@@ -11,6 +10,5 @@ localVue.use(BootstrapVueIcons)
 localVue.use(VueI18n)
 localVue.use(VueRouter)
 localVue.use(Vuex)
-localVue.use(FlashMessage);
 
 export default localVue
