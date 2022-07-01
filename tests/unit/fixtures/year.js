9cc55de0d09fd27fd51e22f82b60742052d06b2f
Richard Jansen <richard@pedroja.tech>
2022-07-01 15:27:52 +0200

    fixture fix

--- tests/unit/fixtures/year.js
+++ tests/unit/fixtures/year.js
@@ -1,30 +1,98 @@
 export default {
-  data: [
-    {
-      "name": "Aboma (10029)",
-      "months": {
-        "10": [
-          {
-            "order_id": 1170,
-            "status": "aangemaakt door AnonymousUser"
+  data: {
+    year_data:
+      [
+        {
+          "name": "Aboma (10029)",
+          "months": {
+            "10": [
+              {
+                "order_id": 1170,
+                "status": "aangemaakt door AnonymousUser"
+              }
+            ]
           }
-        ]
-      }
-    },
-    {
-      "name": "Ambulance Oost (10002)",
-      "months": {
-        "10": [
-          {
-            "order_id": 1132,
-            "status": "aangemaakt door AnonymousUser"
-          },
-          {
-            "order_id": 1131,
-            "status": "aangemaakt door AnonymousUser"
+        },
+        {
+          "name": "Ambulance Oost (10002)",
+          "months": {
+            "10": [
+              {
+                "order_id": 1132,
+                "status": "aangemaakt door AnonymousUser"
+              },
+              {
+                "order_id": 1131,
+                "status": "aangemaakt door AnonymousUser"
+              }
+            ]
           }
-        ]
-      }
+        }
+      ],
+    statuses_data: {
+      "1": {
+        "statuscodes": {
+          "Opdracht gefactureerd": {"count": 45, "perc": 47.87},
+          "opdracht klaar": {"count": 34, "perc": 36.17},
+          "gewijzigd door": {"count": 2, "perc": 2.13},
+          "unknown": {"count": 4, "perc": 4.26},
+          "workorders signed": {"count": 7, "perc": 7.45},
+          "opname offerte": {"count": 1, "perc": 1.06},
+          "Ziek": {"count": 1, "perc": 1.06}
+        }, "total": 94
+      },
+      "2": {
+        "statuscodes": {
+          "opdracht klaar": {"count": 21, "perc": 27.63},
+          "workorders signed": {"count": 23, "perc": 30.26},
+          "unknown": {"count": 2, "perc": 2.63},
+          "Opdracht gefactureerd": {"count": 25, "perc": 32.89},
+          "gewijzigd door": {"count": 2, "perc": 2.63},
+          "aangemaakt": {"count": 1, "perc": 1.32},
+          "opname offerte": {"count": 2, "perc": 2.63}
+        }, "total": 76
+      },
+      "3": {
+        "statuscodes": {
+          "workorders signed": {"count": 52, "perc": 51.49},
+          "Opdracht gefactureerd": {"count": 13, "perc": 12.87},
+          "opdracht klaar": {"count": 25, "perc": 24.75},
+          "unknown": {"count": 8, "perc": 7.92},
+          "gewijzigd door": {"count": 2, "perc": 1.98},
+          "begin opdracht": {"count": 1, "perc": 0.99}
+        }, "total": 101
+      },
+      "4": {
+        "statuscodes": {
+          "workorders signed": {"count": 34, "perc": 44.74},
+          "opdracht klaar": {"count": 31, "perc": 40.79},
+          "Opdracht gefactureerd": {"count": 6, "perc": 7.89},
+          "Opdracht toegewezen": {"count": 3, "perc": 3.95},
+          "unknown": {"count": 2, "perc": 2.63}
+        }, "total": 76
+      },
+      "5": {
+        "statuscodes": {
+          "workorders signed": {"count": 33, "perc": 41.25},
+          "opdracht klaar": {"count": 20, "perc": 25.0},
+          "Opdracht gefactureerd": {"count": 9, "perc": 11.25},
+          "unknown": {"count": 8, "perc": 10.0},
+          "gewijzigd door": {"count": 4, "perc": 5.0},
+          "Opdracht toegewezen": {"count": 6, "perc": 7.5}
+        }, "total": 80
+      },
+      "6": {
+        "statuscodes": {
+          "Opdracht toegewezen": {"count": 7, "perc": 21.88},
+          "workorders signed": {"count": 12, "perc": 37.5},
+          "unknown": {"count": 3, "perc": 9.38},
+          "opdracht klaar": {"count": 6, "perc": 18.75},
+          "gewijzigd door": {"count": 1, "perc": 3.12},
+          "aangemaakt": {"count": 2, "perc": 6.25},
+          "begin opdracht": {"count": 1, "perc": 3.12}
+        }, "total": 32
+      },
+      "7": {"statuscodes": {"unknown": {"count": 1, "perc": 100.0}}, "total": 1}
     }
-  ]
+  }
 }
