c1c520c427e3f38cf45a181411b5fc885a5d1909
Richard Jansen <richard@pedroja.tech>
2022-08-25 17:23:27 +0200

    Changes
    - month stats refactor
    - circleci ci changes
    - version bump

--- tests/unit/fixtures/month.js
+++ tests/unit/fixtures/month.js
@@ -1,81 +1,69 @@
 export default {
   data: {
-    week_data: [
-      {
-        "name": "Aboma (10029)",
-        "weeks": {
-          "43": [
-            {
-              "day": 25,
-              "order_id": 1170,
-              "status": "aangemaakt door AnonymousUser",
-              "order_type": "sales"
-            }
-          ]
-        }
-      },
-      {
-        "name": "Ambulance Oost (10002)",
-        "weeks": {
-          "43": [
-            {
-              "day": 27,
-              "order_id": 1131,
-              "status": "aangemaakt door AnonymousUser",
-              "order_type": "sales"
-            },
-            {
-              "day": 26,
-              "order_id": 1132,
-              "status": "aangemaakt door AnonymousUser",
-              "order_type": "sales"
-            }
-          ]
-        }
-      }
-    ],
-    "statuses_data": {
-      "22": {
-        "statuscodes": {
-          "workorders signed": {
-            "count": 2,
-            "perc": 0.6666666666666666
-          }, "opdracht klaar": {"count": 1, "perc": 0.3333333333333333}
-        }, "total": 3
-      },
-      "23": {
-        "statuscodes": {
-          "unknown": {"count": 3, "perc": 0.17647058823529413},
-          "opdracht klaar": {"count": 4, "perc": 0.23529411764705882},
-          "workorders signed": {"count": 7, "perc": 0.4117647058823529},
-          "gewijzigd door": {"count": 1, "perc": 0.058823529411764705},
-          "begin opdracht": {"count": 1, "perc": 0.058823529411764705},
-          "aangemaakt": {"count": 1, "perc": 0.058823529411764705}
-        }, "total": 17
-      },
-      "24": {
-        "statuscodes": {
-          "Opdracht toegewezen": {"count": 7, "perc": 0.5833333333333334},
-          "workorders signed": {"count": 3, "perc": 0.25},
-          "aangemaakt": {"count": 1, "perc": 0.08333333333333333},
-          "opdracht klaar": {"count": 1, "perc": 0.08333333333333333}
-        }, "total": 12
+    "month_data": {
+      "total": 81,
+      "weeks": {
+        "32": {"count": 23, "perc": "29.11"},
+        "33": {"count": 19, "perc": "23.46"},
+        "31": {"count": 33, "perc": "84.62"},
+        "34": {"count": 5, "perc": "7.14"},
+        "35": {"count": 1, "perc": "0.00"}
       }
     },
+    "statuses_data": {
+      "31": {
+        "statuscodes": {
+          "opdracht klaar": {"count": 5, "perc": 15.15},
+          "workorders signed": {"count": 27, "perc": 81.82},
+          "gewijzigd door": {"count": 1, "perc": 3.03}
+        }, "total": 33
+      },
+      "32": {
+        "statuscodes": {
+          "workorders signed": {"count": 15, "perc": 65.22},
+          "opdracht klaar": {"count": 7, "perc": 30.43},
+          "Opdracht toegewezen": {"count": 1, "perc": 4.35}
+        }, "total": 23
+      },
+      "33": {
+        "statuscodes": {
+          "Opdracht toegewezen": {"count": 17, "perc": 89.47},
+          "planning": {"count": 2, "perc": 10.53}
+        }, "total": 19
+      },
+      "34": {
+        "statuscodes": {
+          "Opdracht toegewezen": {"count": 3, "perc": 60.0},
+          "planning": {"count": 1, "perc": 20.0},
+          "gewijzigd door": {"count": 1, "perc": 20.0}
+        }, "total": 5
+      },
+      "35": {"statuscodes": {"gewijzigd door": {"count": 1, "perc": 100.0}}, "total": 1}
+    },
     "assigned_orders_data": {
-      "27": {
-        "total": 16,
+      "32": {
+        "total": 22,
         "data": {
-          "1": {"count": 10, "perc": 62.0},
-          "2": {"count": 3, "perc": 19.0},
-          "0": {"count": 3, "perc": 19.0}
+          "18": {"count": 1, "perc": 5.0},
+          "2": {"count": 1, "perc": 5.0},
+          "1": {"count": 19, "perc": 86.0},
+          "3": {"count": 1, "perc": 5.0}
         }
       },
-      "26": {"total": 4, "data": {"1": {"count": 4, "perc": 100.0}}},
-      "29": {"total": 1, "data": {"1": {"count": 1, "perc": 100.0}}},
-      "28": {
-        "total": 0, "data": {}
-      }
+      "33": {
+        "total": 18,
+        "data": {
+          "1": {"count": 16, "perc": 89.0},
+          "2": {"count": 1, "perc": 6.0},
+          "0": {"count": 1, "perc": 6.0}
+        }
+      },
+      "31": {
+        "total": 32,
+        "data": {"2": {"count": 7, "perc": 22.0}, "1": {"count": 25, "perc": 78.0}}
+      },
+      "34": {"total": 4, "data": {"1": {"count": 4, "perc": 100.0}}},
+      "35": {"total": 0, "data": {}}
     }
   }
 }
