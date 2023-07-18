d3faf6a01086c2cb5ac5c89a2904930a5b3c0dc3
ikbensiep <ikbensiep>
2023-07-18 18:59:46 +0200

    Update Order Details pages layout / markup workorder dialog instead of new browser tab (WIP) minor cleanup

--- src/views/orders/OrderViewMaintenance.vue
+++ src/views/orders/OrderViewMaintenance.vue
@@ -1,189 +1,157 @@
 <template>
-  <b-overlay :show="isLoading" rounded="sm">
-    <div class="app-detail" v-if="order">
-      <h3>{{ $trans('Order info') }}</h3>
-      <b-row>
-        <b-col cols="6">
-          <b-table-simple>
-            <b-tr>
-              <b-td><strong>{{ $trans('Customer ID') }}:</strong></b-td>
-              <b-td>{{ order.customer_id }}</b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Customer') }}:</strong></b-td>
-              <b-td>{{ order.order_name }}</b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Address') }}:</strong></b-td>
-              <b-td>{{ order.order_address }}</b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Country/Postal/city') }}:</strong></b-td>
-              <b-td>
-                {{ order.order_country_code }}-
-                {{ order.order_postal }} {{ order.order_city }}
-              </b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Contact') }}:</strong></b-td>
-              <b-td>{{ order.order_contact }}</b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Tel') }}:</strong></b-td>
-              <b-td>{{ order.order_tel }}</b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Mobile') }}:</strong></b-td>
-              <b-td>{{ order.order_mobile }}</b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Email') }}:</strong></b-td>
-              <b-td>
-                <b-link class="px-1" v-bind:href="`mailto:${order.order_email}`">
-                  {{ order.order_email }}
-                </b-link>
-              </b-td>
-            </b-tr>
-          </b-table-simple>
-        </b-col>
-        <b-col cols="6">
-          <b-table-simple>
-            <b-tr>
-              <b-td><strong>{{ $trans('Order id') }}:</strong></b-td>
-              <b-td>{{ order.order_id }}</b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Date') }}:</strong></b-td>
-              <b-td>{{ order.order_date }}</b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Service nbr.') }}:</strong></b-td>
-              <b-td>{{ order.service_number }}</b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Order reference') }}:</strong></b-td>
-              <b-td>{{ order.order_reference }}</b-td>
-            </b-tr>
-            <b-tr>
-              <b-td><strong>{{ $trans('Order type') }}:</strong></b-td>
-              <b-td>{{ order.order_type }}</b-td>
-            </b-tr>
-          </b-table-simple>
-        </b-col>
-      </b-row>
-      <b-row v-if="order.orderlines.length">
-        <b-col cols="12">
-          <h4>{{ $trans('Orderlines') }}</h4>
-          <b-table
-            dark
-            borderless
-            small
-            id="orderline-table"
-            :fields="orderLineFields"
-            :items="order.orderlines"
-            responsive="sm"
-          ></b-table>
-          </b-col>
-      </b-row>
-      <b-row v-if="!isCustomer && !hasBranches && order.infolines.length > 0">
-        <b-col cols="12">
-          <h4>{{ $trans('Infolines') }}</h4>
-          <b-table dark borderless small :fields="infoLineFields" :items="order.infolines" responsive="sm"></b-table>
-        </b-col>
-      </b-row>
-      <b-row class="my-2" v-if="order.customer_remarks != ''">
-          <b-col cols="2"><strong>{{ $trans('Customer remarks') }}</strong></b-col>
-          <b-col cols="9">{{ order.customer_remarks }}</b-col>
-      </b-row>
-      <b-row class="my-2">
-          <b-col cols="2"><strong>{{ $trans('Status') }}</strong></b-col>
-          <b-col cols="9">
-            <div v-for="status in order.statuses" :key="status.id">
-              {{ status.created }} {{ status.status }}<br/>
+  <div v-if="order">
+    <b-modal ref="workorder-viewer" id="workorder-viewer" size="xl" scrollable title="Workorder">
+      <iframe :src="this.workorderURL" style="min-height:720px; width: 100%;"></iframe>
+      <template #modal-footer="{ ok }">
+        <div class="flex-columns">
+          <b-button size="sm">download pdf</b-button>
+          <b-button size="sm">print</b-button>
+          <!-- Emulate built in modal footer ok and cancel button actions -->
+          <b-button size="sm" @click="ok()" variant="primary">
+            close
+          </b-button>
+        </div>
+      </template>
+    </b-modal>
+    <div class="flex-columns wrap">
+      <div class="panel">
+        <h6>
+          <strong>{{ order.order_name }}</strong> <em>{{ order.order_type }}</em>
+          <div v-if="isLoading">Loading</div>
+        </h6>
+        <dl>
+          <dt>Dates</dt>
+          <dd>{{ order.order_date }}</dd>
+          <dt>Service &numero;</dt>
+          <dd>{{ order.service_number }}</dd>
+          <dt>Reference</dt>
+          <dd>{{ order.order_reference }}</dd>
+          <dt v-if="order.customer_remarks">Remarks</dt>
+          <dd v-if="order.customer_remarks">{{ order.customer_remarks }}</dd>
+        </dl>
+        <hr>
+
+        <h6>Workorder  {{ order.order_id }}</h6>
+          <div class="flex-columns">
+            <b-link class="" @click="openWorkorder()" target="_blank">
+              <b-icon icon="file-earmark"></b-icon>{{ $trans('View workorder') }}
+            </b-link>
+            <button @click="showWorkorderDialog()"></button>
+            &nbsp;
+            <b-link class="" :href="order.workorder_pdf_url" target="_blank" :title="$trans('Download PDF') + ' (' + order.workorder_pdf_url + ')'">
+              <b-icon icon="file-earmark-pdf"></b-icon>{{ $trans('Download PDF') }}
+            </b-link>
+          </div>
+          <br>
+          <div class="flex-columns">
+            <b-button
+              v-if="!past && !isCustomer && !isBranchEmployee"
+              id="recreateWorkorderPdfButton"
+              @click="recreateWorkorderPdf"
+              :disabled="buttonDisabled"
+              class="btn btn-secondary"
+              type="button"
+              variant="secondary"
+            >
+              {{ $trans('Generate PDF') }}
+            </b-button>
+            <b-button
+              v-if="!past && !isCustomer && !isBranchEmployee"
+              id="recreateWorkorderPdfButtonGotenberg"
+              @click="recreateWorkorderPdfGotenberg"
+              :disabled="buttonDisabled"
+              class="btn btn-secondary"
+              type="button"
+              variant="secondary"
+            >
+              {{ $trans('re-generate PDF (new)') }}
+            </b-button>
+          </div>
+        <hr>
+
+        <h6>{{ $trans('Documents') }}</h6>
+        <div class="my-2" v-if="order.workorder_documents.length > 0">
+          <ul class="listing">
+            <li v-for="doc in order.workorder_documents" :key="doc.url">
+              <a class="listing-item" :href="doc.url" target="_blank">
+                <span>{{ doc.name}}</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div v-else>
+          <p>No documents. </p>
+        </div>
+
+        <h6>Orderlines</h6>
+        <ul class="listing" v-if="order.orderlines.length">
+          <li v-for="line in order.orderlines" :key="line.id">
+            <div class="listing-item flex-columns">
+              <div>{{ line.product  }}</div>
+              <div>{{ line.remarks  }}</div>
+              <div>{{ line.location  }}</div>
             </div>
+          </li> 
+        </ul>
+      </div>
+
+      <div class="panel">
+        <h6>Timeline</h6>
+        <ul class="listing" style="max-height: 75vh; overflow: auto;">
+          <li v-for="status in order.statuses.slice().reverse()" :key="status.id">
+            <div class="listing-item">
+              <span>{{ status.status }}</span>
+              <span class="dimmed">{{ status.created }}</span> 
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div class="panel">
+        <h6>Contact</h6>
+        <p>
+          {{ order.order_contact }}<br/>
+          {{ order.order_tel }} {{ order.order_mobile }}<br/>
+          <b-link v-bind:href="`mailto:${order.order_email}`">{{ order.order_email }}</b-link>
+        </p>
+        <hr>
+        <address>
+          {{ order.order_name }}<br />
+          {{ order.order_address }}<br />
+          {{ order.order_postal }}<br />
+          {{ order.order_city }}, {{ order.order_country_code }}
+        </address>
+        <hr>
+        <b-table  v-if="!isCustomer && !hasBranches && order.infolines.length > 0" borderless small :fields="infoLineFields" :items="order.infolines" responsive="sm"></b-table>
+        
+        <b-row class="my-2" v-if="order.workorder_documents_partners && order.workorder_documents_partners.length > 0">
+          <b-col cols="12">
+            <h6>{{ $trans('Workorder documents partner') }}</h6>
+            <b-table borderless small :fields="workorderDocumentFields" :items="order.workorder_documents_partners" responsive="sm">
+              <template #cell(url)="data">
+                <b-link class="px-1" :href="data.item.url" target="_blank">
+                  {{ $trans('Order') }} {{ order.order_id }}
+                </b-link>
+              </template>
+            </b-table>
+          </b-col>
+        </b-row>
+        <b-row v-if="order.reported_codes_extra_data.length">
+          <b-col cols="12">
+            <h6>{{ $trans('Reported extra text') }}</h6>
+            <b-table
+              borderless
+              small
+              id="extra-data-table"
+              :fields="extraDataFields"
+              :items="order.reported_codes_extra_data"
+              responsive="sm"
+            ></b-table>
           </b-col>
       </b-row>
-      <b-row class="my-2">
-          <b-col cols="2"><strong>{{ $trans('Workorder online') }}</strong></b-col>
-          <b-col cols="9">
-            <b-link class="px-1" @click="openWorkorder()" target="_blank">
-              {{ $trans('Order') }} {{ order.order_id }}
-            </b-link>
-          </b-col>
-      </b-row>
-      <b-row class="my-2" v-if="order.workorder_pdf_url">
-          <b-col cols="2"><strong>{{ $trans('Download PDF') }}</strong></b-col>
-          <b-col cols="9">
-            <b-link class="px-1" :href="order.workorder_pdf_url" target="_blank">
-              {{ $trans('Order') }} {{ order.order_id }} |{{ order.workorder_pdf_url }}|
-            </b-link>
-          </b-col>
-      </b-row>
-      <b-row class="my-2" v-if="order.workorder_documents.length > 0">
-        <b-col cols="12">
-          <h4>{{ $trans('Workorder documents') }}</h4>
-          <b-table borderless small :fields="workorderDocumentFields" :items="order.workorder_documents" responsive="sm">
-            <template #cell(url)="data">
-              <b-link class="px-1" :href="data.item.url" target="_blank">
-                {{ $trans('Order') }} {{ order.order_id }}
-              </b-link>
-            </template>
-          </b-table>
-        </b-col>
-      </b-row>
-      <b-row class="my-2" v-if="order.workorder_documents_partners && order.workorder_documents_partners.length > 0">
-        <b-col cols="12">
-          <h4>{{ $trans('Workorder documents partner') }}</h4>
-          <b-table borderless small :fields="workorderDocumentFields" :items="order.workorder_documents_partners" responsive="sm">
-            <template #cell(url)="data">
-              <b-link class="px-1" :href="data.item.url" target="_blank">
-                {{ $trans('Order') }} {{ order.order_id }}
-              </b-link>
-            </template>
-          </b-table>
-        </b-col>
-      </b-row>
-      <b-row v-if="order.reported_codes_extra_data.length">
-        <b-col cols="12">
-          <h4>{{ $trans('Reported extra text') }}</h4>
-          <b-table
-            borderless
-            small
-            id="extra-data-table"
-            :fields="extraDataFields"
-            :items="order.reported_codes_extra_data"
-            responsive="sm"
-          ></b-table>
-        </b-col>
-      </b-row>
-      <footer class="modal-footer">
-        <b-button
-          v-if="!past && !isCustomer && !isBranchEmployee"
-          id="recreateWorkorderPdfButton"
-          @click="recreateWorkorderPdf"
-          :disabled="buttonDisabled"
-          class="btn btn-danger"
-          type="button"
-          variant="secondary"
-        >
-          {{ $trans('Recreate workorder PDF') }}
-        </b-button>
-        <b-button
-          v-if="!past && !isCustomer && !isBranchEmployee"
-          id="recreateWorkorderPdfButtonGotenberg"
-          @click="recreateWorkorderPdfGotenberg"
-          :disabled="buttonDisabled"
-          class="btn btn-danger"
-          type="button"
-          variant="secondary"
-        >
-          {{ $trans('Recreate workorder PDF (new)') }}
-        </b-button>
-        <b-button @click="goBack" class="btn btn-info" type="button" variant="primary">
-          {{ $trans('Back') }}</b-button>
-      </footer>
+      </div>
     </div>
-  </b-overlay>
+  </div>
 </template>
 
 <script>
@@ -197,6 +165,7 @@ export default {
       isLoading: false,
       buttonDisabled: false,
       order: orderModel.getFields(),
+      workorderURL: '',
       orderLineFields: [
         { key: 'product', label: this.$trans('Product') },
         { key: 'location', label: this.$trans('Location') },
@@ -229,6 +198,14 @@ export default {
     openWorkorder() {
       const routeData = this.$router.resolve({ name: 'workorder-view', params: { uuid: this.order.uuid } })
       window.open(`${document.location.origin}/${routeData.href}`, '_blank')
+    },
+    getWorkorderURL() {
+      const routeData = this.$router.resolve({ name: 'workorder-view', params: { uuid: this.order.uuid } })
+      return `${document.location.origin}/${routeData.href}`;
+    },
+    showWorkorderDialog() {
+      this.workorderURL = this.getWorkorderURL();
+      this.$refs['workorder-viewer'].show()
     },
     async recreateWorkorderPdf() {
       this.isLoading = true
@@ -264,9 +241,6 @@ export default {
         this.isLoading = false
       }
     },
-    goBack() {
-      this.$router.go(-1)
-    },
     async loadOrder() {
       this.isLoading = true
 
@@ -281,7 +255,7 @@ export default {
     }
   },
   created() {
-    this.loadOrder()
+    this.loadOrder();
   }
 }
 </script>
