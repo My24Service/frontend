9768915b64fa83c1f6c0f25297e16f317fee03ad
ikbensiep <ikbensiep>
2023-07-18 18:57:06 +0200

    Update OrderForm markup

--- src/views/orders/OrderForm.vue
+++ src/views/orders/OrderForm.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
-    <OrderFormMaintenance
-      v-if="memberType === 'maintenance'"
-      :pk="pk"
-      :unaccepted="unaccepted"
-      :maintenance="maintenance"
-    />
-    <OrderFormTemps v-if="memberType === 'temps'" :pk="pk" />
+  <div class="app-page">
+    <header>
+      <div class='page-title'>
+        <h3 v-if="!pk">
+          <b-icon icon="clipboard"></b-icon>
+          <router-link :to="{name:'order-list'}">Orders</router-link> / 
+          <strong>new order</strong>
+        </h3>
+        <h3 v-if="pk">
+          <b-icon icon="clipboard"></b-icon>
+          <router-link :to="{name:'order-list'}">Orders</router-link> / 
+          <router-link :to="{name: 'order-view', pk:pk}">#{{ pk }}</router-link>
+         / <strong>edit</strong>
+        </h3>
+      </div>
+    </header>
+    <div class="app-detail">
+      <OrderFormMaintenance
+        v-if="memberType === 'maintenance'"
+        :pk="pk"
+        :unaccepted="unaccepted"
+        :maintenance="maintenance"
+      />
+      <OrderFormTemps v-if="memberType === 'temps'" :pk="pk" />
+    </div>
   </div>
 </template>
 
