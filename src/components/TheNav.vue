f8cc04db0cf746d603feefefc1e5756d99d12e88
ikbensiep <ikbensiep>
2023-07-18 18:17:37 +0200

    Change app layout, move subnav into sidebar menu, rename TheNavNotLoggedIn -> TheNavLoggedOut

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,24 +1,22 @@
 <template>
-	<div ref='nav-container'>
-		<TheNavNotLoggedIn v-if="!isLoggedIn" />
+	<div ref='nav-container' class="nav-container">
 		<TheNavLoggedIn v-if="isLoggedIn" />
+		<TheNavLoggedOut v-if="!isLoggedIn" />
 	</div>
 </template>
 
 <script>
 import { componentMixin } from '@/utils.js'
-import TheNavNotLoggedIn from '@/components/TheNavNotLoggedIn.vue'
+import TheNavLoggedOut from '@/components/TheNavLoggedOut.vue'
 import TheNavLoggedIn from '@/components/TheNavLoggedIn.vue'
 
 export default {
   name: "TheNav",
   mixins: [componentMixin],
 	components: {
-	  TheNavNotLoggedIn,
+	  TheNavLoggedOut,
 	  TheNavLoggedIn,
 	},
 }
 </script>
 
-<style scoped>
-</style>
