1aebda0325a12b04ea3df8c4b7a69010bcea9fb7
ikbensiep <ikbensiep>
2023-07-20 16:26:25 +0200

    Add separate, non-modal search form (WIP)

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,38 @@
+<template>  
+  <form ref="search-form" @submit.stop.prevent="doSearch">
+      <b-form-input
+        size="sm"
+        autofocus
+        v-model="query"
+        :placeholder="placeholderText"
+      ></b-form-input>
+      
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholderText: String || undefined
+  },
+  data() {
+    return {
+      query: null
+    }
+  },
+  methods: {
+    doSearch() {
+      this.$emit('do-search', this.query)
+    },
+    show() {
+      this.$refs['search-modal'].show()
+    },
+    hide() {
+      this.$refs['search-modal'].hide()
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
