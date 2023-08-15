081c6b0f62e333aa03ceffbde3e7af51d6f5095e
ikbensiep <ikbensiep>
2023-08-15 17:08:28 +0200

    Only show logo in logged out nav

--- src/components/TheNavLoggedOut.vue
+++ src/components/TheNavLoggedOut.vue
@@ -1,56 +1,27 @@
 <template>
   <div>
-    <LoginModal ref="login-modal-component" />
-
-    <b-navbar toggleable="sm" variant="primary">
-      <b-container>
-
-        <NavBrand />
-
-        <b-collapse id="nav-collapse" is-nav>
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item @click="showLogin">Login</b-nav-item>
-            <TheLanguageChooser />
-          </b-navbar-nav>
-        </b-collapse>
-
-        <Version />
-
-      </b-container>
-    </b-navbar>
+    <NavBrand />
   </div>
 </template>
 
 <script>
-import LoginModal from "./LoginModal.vue"
-import TheLanguageChooser from "./TheLanguageChooser.vue"
 import NavBrand from "./NavBrand.vue"
-import Version from "./Version.vue"
 
 export default {
   components: {
-    TheLanguageChooser,
     NavBrand,
-    Version,
-    LoginModal,
   },
-  name: "TheNavNotLoggedIn",
+  name: "TheNavLoggedOut",
   data() {
     return {
     }
-  },
-  methods: {
-    showLogin() {
-      this.$refs['login-modal-component'].show()
-    },
   }
 }
 
 </script>
 
 <style scoped>
-.navbar {
-  padding: 0 !important;
+div {
+  text-align: center;
 }
 </style>
