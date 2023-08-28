8f5ff344fb48663450c751e776d4ef2a73e200a9
Richard Jansen <richard@pedroja.tech>
2023-08-28 09:37:21 +0200

    added test for Materials component; changed prices

--- tests/unit/views/orders/invoice-form.spec.js
+++ tests/unit/views/orders/invoice-form.spec.js
@@ -278,4 +278,134 @@ describe('Distance', () => {
     const els = wrapper.findAll('.distance_row')
     expect(els.length).to.equal(3)
   })
+
+  it('renders the total distance', async () => {
+    const wrapper = mount(Distance, {
+      localVue,
+      store,
+      mocks: {
+        $trans: (f) => f
+      },
+      propsData: {
+        user_totals: invoiceResponse.data.activity_totals.user_totals,
+        engineer_models: invoiceResponse.data.engineer_models,
+        customer: new CustomerModel(customerResponse.data),
+        distance_total: invoiceResponse.data.activity_totals.distance_total,
+        invoice_default_price_per_km: '1.00'
+      }
+    })
+
+    await flushPromises()
+
+    const el = wrapper.find('.items-total')
+    expect(el.html()).to.contain(invoiceResponse.data.activity_totals.distance_total)
+  })
+
+  it('renders a very big total distance and VAT', async () => {
+    const digit = 1024 * 1024
+    // const digit = 1024 * 1024 * 0.1 // ok
+    // const digit = 1024 * 1024 * 0.01 // yields vat: expected '€823001.08' to include '823001.09'
+    // const digit = 1024 * 1024 * 1.10001 // yields vat: expected '€90530942.69' to include '90530942.68'
+    // const digit = 1024 * 1024 * 1.11 // yields vat: expected '€91353120.76' to include '91353120.77'
+    // just to remember, it won't happen because distance is an int in the backend
+
+    const total = invoiceResponse.data.activity_totals.user_totals.reduce(
+      (total, m) => ( total + (m.distance_total * digit * 1.15 * 100)),
+      0
+    )
+
+    const vat = invoiceResponse.data.activity_totals.user_totals.reduce(
+      (total, m) => ( total + (m.distance_total * digit * 0.21 * 1.15 * 100)),
+      0
+    )
+
+    const wrapper = mount(Distance, {
+      localVue,
+      store,
+      mocks: {
+        $trans: (f) => f
+      },
+      propsData: {
+        user_totals: invoiceResponse.data.activity_totals.user_totals.map((m) => ({
+          ...m,
+          distance_total: m.distance_total * digit
+        })),
+        engineer_models: invoiceResponse.data.engineer_models,
+        customer: new CustomerModel(customerResponse.data),
+        distance_total: invoiceResponse.data.activity_totals.distance_total * digit,
+        invoice_default_price_per_km: '1.15'
+      }
+    })
+
+    await flushPromises()
+
+    const el = wrapper.find('.items-total')
+    expect(el.html()).to.contain(invoiceResponse.data.activity_totals.distance_total * digit)
+
+    const total_input = wrapper.find('input.total-input-final')
+    expect(total_input.element.value).to.contain((total/100).toFixed(2))
+
+    const vat_input = wrapper.find('input.vat-input-final')
+    expect(vat_input.element.value).to.contain((vat/100).toFixed(2))
+  })
+
+})
+
+describe('Materials', () => {
+  let store
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      getters,
+    })
+  })
+
+  it('has 6 headers', async () => {
+    const wrapper = shallowMount(Materials, {
+      localVue,
+      store,
+      mocks: {
+        $trans: (f) => f
+      },
+      propsData: {
+        material_models: invoiceResponse.data.material_models,
+        used_materials: invoiceResponse.data.used_materials,
+        customer: new CustomerModel(customerResponse.data)
+      }
+    })
+
+    await flushPromises()
+
+    const trs = wrapper.findAll('.header')
+    expect(trs.length).to.equal(6)
+  })
+
+  it('has a total of €20.50 and VAT €4.30', async () => {
+    const wrapper = mount(Materials, {
+      localVue,
+      store,
+      mocks: {
+        $trans: (f) => f
+      },
+      propsData: {
+        material_models: invoiceResponse.data.material_models,
+        used_materials: invoiceResponse.data.used_materials,
+        customer: new CustomerModel(customerResponse.data)
+      }
+    })
+
+    // 4x 947 a 1.25 = 5, vat 1.05
+    // 4x 1001 a 0.75 = 3, vat 0.63
+    // 50x 955 a 0.25 = 12.5, vat 2.63
+    // total 20.5, vat 4.30
+
+    await flushPromises()
+
+    const total_input = wrapper.find('input.total-input-final')
+    expect(total_input.element.value).to.equal('€20.50')
+
+    const vat_input = wrapper.find('input.vat-input-final')
+    expect(vat_input.element.value).to.contain("€4.30")
+  })
+  //
 })
