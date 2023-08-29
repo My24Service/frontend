c9d24a794e9be4f7b73d3ec7bf3a36919a3374c8
Sibrand Hoekstra <sibrand@gmail.com>
2023-08-29 12:11:44 +0200

    Move deleteOrder+modal to OrderTableInfo

--- src/components/OrderTableInfo.vue
+++ src/components/OrderTableInfo.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="listing-item">
+    <!-- delete order modal -->
+    <b-modal
+      v-if="!isCustomer && !isBranchEmployee"
+      id="delete-order-modal"
+      ref="delete-order-modal"
+      v-bind:title="$trans('Delete?')"
+      @ok="this.doDelete"
+    >
+      <p class="my-4">{{ $trans('Are you sure you want to delete this order?') }}</p>
+    </b-modal>
     <router-link v-if="isLoaded" :to="{name: 'order-view', params: {pk: order.id}}" class="order-id">
         #{{ order.order_id }}
     </router-link>
     <router-link v-if="isLoaded" :to="{name: 'order-view', params: {pk: order.id}}" class="order-type">
       <strong>{{ order.order_type }}</strong>
     </router-link>
-    
+
     <span class="order-company-name">{{ order.order_name }}</span>
-    
+
     <span class="order-start-date" :title="`${order.start_date} ${order.start_time ? ' ' + order.start_time :'' }`">
-      {{ order.start_date }} 
+      {{ order.start_date }}
       <b v-if="order.start_time !== null" :title="order.start_time"><b-icon icon="clock"></b-icon></b>
     </span>
 
       <!-- fixme -->
       <span v-if="memberType === 'maintenance'" class="order-assignees">
         <span v-if="order.assigned_count" :title="`assignees: ${order.required_assigned}`">
-          {{ $trans('Assigned to') }} {{ order.assigned_count }} 
+          {{ $trans('Assigned to') }} {{ order.assigned_count }}
           <span v-if="order.assigned_count > 1">
             <span v-if="order.required_users"> / {{ order.required_users }}</span>
             people
@@ -26,8 +36,8 @@
         </span>
         <span v-else title="Not assigned to anyone">&ndash;</span>
       </span>
-      <span>
-        <span v-if="order.documents.length">
+      <span class="order-documents">
+        <span v-if="order.documents.length" >
           <b-icon icon="paperclip"></b-icon>
         </span>
         <router-link v-if="isLoaded && order.documents.length" :to="{name: 'order-documents', params: {orderPk: order.id}}" class="order-type">
@@ -48,6 +58,11 @@
           style="border-color: transparent;"
           @change="handleStatusChange(order.id, $event)"
         ></b-form-select>
+        <IconLinkDelete
+          v-if="!isCustomer && !isBranchEmployee"
+          v-bind:title="$trans('Delete')"
+          v-bind:method="this.showDeleteModal"
+        />
       </span>
   </div>
 </template>
@@ -64,9 +79,13 @@
 import my24 from '../services/my24.js'
 import { componentMixin } from '../utils.js'
 import statusModel from '@/models/orders/Status.js'
+import IconLinkDelete from './IconLinkDelete.vue'
 
 export default {
   mixins: [componentMixin],
+  components: {
+    IconLinkDelete
+  },
   async created() {
     this.memberType = await this.$store.dispatch('getMemberType')
     this.statuscodes = await this.$store.dispatch('getStatuscodes')
@@ -111,9 +130,26 @@ export default {
     order: {
       type: [Object],
       required: true
+    },
+    model: {
+      type: [Object],
     }
-  }, 
+  },
   methods: {
+    showDeleteModal() {
+      console.info('show modal')
+      this.$refs['delete-order-modal'].show()
+    },
+    async doDelete() {
+      try {
+        await this.model.delete(this.order.id)
+        this.infoToast(this.$trans('Deleted'), this.$trans('Order has been deleted'))
+        this.$emit('reload-data')
+      } catch(error) {
+        console.log('Error deleting order', error)
+        this.errorToast(this.$trans('Error deleting order'))
+      }
+    },
     handleStatusChange(id, value) {
       this.changeStatus(id, value);
       this.orderStatusColorCode = my24.status2color(this.statuscodes, value);
