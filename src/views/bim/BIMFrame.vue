75ef86ba341eaa9a9d49359b8a5e1040fe5792e0
ikbensiep <ikbensiep>
2023-10-11 14:40:58 +0200

    Add webshop+BIM iframe templates

---
+++ src/views/bim/BIMFrame.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='app-page'>
+    <header>
+      <div class='page-title'>
+        <h3><b-icon icon="mouse2-fill"></b-icon> BIM</h3>
+      </div>
+    </header>
+    <div class='page-detail'>
+      <div class='panel'>
+        <iframe src="https://4indoor.iv.navvis.com/"></iframe>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+iframe {
+  width: 100%;
+  min-height: 50vh;
+  border: none;
+}
+</style>
+
+<script>
+export default {
+  name: 'BIMFrame'
+}
+</script>
---
+++ src/views/webshop/WebshopIframe.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class='app-page'>
+    <header>
+      <div class='page-title'>
+        <h3><b-icon icon="basket"></b-icon> Webshop</h3>
+      </div>
+    </header>
+    <div class='page-detail'>
+      <div class='panel'>
+        <iframe src=""></iframe>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+iframe {
+  background-color: #0001;
+  width: 100%;
+  min-height: 50vh;
+  border: none;
+}
+</style>
+
+<script>
+export default {
+  name: 'WebshopIframe'
+}
+</script>
