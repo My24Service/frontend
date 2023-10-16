8e674039bc73ad46fb08bfc40b837260425647e2
Richard Jansen <richard@pedroja.tech>
2023-10-16 11:21:20 +0200

    added shell script to deploy siep's branch

---
+++ siep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BRANCH="app-layout-merge-dev"
+HOST="139.162.254.243"
+
+cd /tmp
+rm -fr pt-vue
+git clone git@scm.platform.my24service-dev.com:services/pt-vue.git
+cd pt-vue
+git co $BRANCH
+git pull origin $BRANCH
+npm install
+npm run build
+tar zcf siep.tgz dist
+scp siep.tgz  jenkins@$HOST:~/
+ssh jenkins@my24-dev tar zxf siep.tgz  -C /var/www/my24service/shltr
+rm siep.tgz
