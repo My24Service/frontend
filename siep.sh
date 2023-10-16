c7ccf570b1532dc0161ad90502d20457e31722ca
Richard Jansen <richard@pedroja.tech>
2023-10-16 16:23:20 +0200

    cleanup target dir fist

--- siep.sh
+++ siep.sh
@@ -13,5 +13,6 @@ npm install
 npm run build
 tar zcf siep.tgz dist
 scp siep.tgz  jenkins@$HOST:~/
+ssh jenkins@$HOST rm -fr //var/www/my24service/shltr/*
 ssh jenkins@$HOST tar zxf siep.tgz  -C /var/www/my24service/shltr
 rm siep.tgz
