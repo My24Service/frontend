068d841fb5fdc4865f786297b23bb48850bcafc8
Richard Jansen <richard@pedroja.tech>
2023-11-13 12:43:12 +0100

    repo change

--- siep-live.sh
+++ siep-live.sh
@@ -2,11 +2,12 @@
 
 BRANCH="app-layout-merge-dev"
 HOST="178.79.153.236"
+REPO_NAME="frontend"
 
 cd /tmp
-rm -fr pt-vue
-git clone git@scm.platform.my24service-dev.com:services/pt-vue.git
-cd pt-vue
+rm -fr $REPO_NAME
+git clone git@github.com:My24Service/$REPO_NAME.git
+cd $REPO_NAME
 git checkout $BRANCH
 git pull origin $BRANCH
 npm install
