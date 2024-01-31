07298ba4f2507cd5673b37146314b0c6b31a0b9b
Richard Jansen <richard@pedroja.tech>
2024-01-31 13:08:49 +0100

    enabled customer import for testing on dev

--- src/components/SubNavCustomers.vue
+++ src/components/SubNavCustomers.vue
@@ -32,7 +32,7 @@
       </b-nav-item>
       <b-nav-item
         :active="isActive('import')"
-        v-if="hasUpload && false"
+        v-if="hasUpload"
         :to="{ name: 'customer-import-list' }">
         {{ $trans('Import') }}
       </b-nav-item>
