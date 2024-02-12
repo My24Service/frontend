82ffe3c18a2352feae97c6167a7bf206ae744cb9
Richard Jansen <richard@pedroja.tech>
2024-02-12 18:47:29 +0100

    bugfix in download file

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,14 @@ import axios from 'axios'
 import BASE_URL from './base-url'
 import auth from '@/services/auth'
 
-let client
+let client, normalClient
 
 if (process.env.NODE_ENV !== 'test') {
   client = axios.create({
     baseURL: `${BASE_URL}/api`,
+    withCredentials: false
+  })
+  normalClient = axios.create({
     withCredentials: false
   })
   auth.setInterceptors(client)
@@ -15,3 +18,4 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 export default client
+export {normalClient}
