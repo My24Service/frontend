c48a9bb0b5f177113e7caeb9fbc0850a11ae9bd1
Eniola Arinde <eniolaarinde1@gmail.com>
2024-04-26 10:56:15 +0100

    Add templates route to parts alowed

--- src/services/my24.js
+++ src/services/my24.js
@@ -153,7 +153,7 @@ class My24 extends BaseModel {
 
     const parts_always_allowed = [
       'form', 'view', 'info', 'company', 'activity', 'pictures',
-      'planning-users', 'employee-users', 'import', 'statuscodes',
+      'planning-users', 'employee-users', 'import', 'statuscodes', 'templates'
     ]
     if (parts_always_allowed.indexOf(config.part) !== -1) {
       if (debug) console.debug(`allowed: part "${config.part}" in always allowed (${parts_always_allowed.join('/')})`)
