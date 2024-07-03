2c8c402a9e3d7b647f1bf19c31fe69623645c5a5
Richard Jansen <demo_epso@proton.me>
2024-07-03 14:01:43 +0200

    always allow map

--- src/services/my24.js
+++ src/services/my24.js
@@ -164,7 +164,7 @@ class My24 extends BaseModel {
     const parts_always_allowed = [
       'form', 'view', 'info', 'company', 'activity', 'pictures',
       'planning-users', 'employee-users', 'import', 'statuscodes',
-      'api-users'
+      'api-users', 'map'
     ]
     if (parts_always_allowed.indexOf(config.part) !== -1) {
       if (debug) console.debug(`allowed: part "${config.part}" in always allowed (${parts_always_allowed.join('/')})`)
