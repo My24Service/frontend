739017dd2e462a064ea9d54f28cbf028f222f890
Hugo van Galen <hugovangalen@gmail.com>
2026-03-08 14:12:59 +0100

    Add changes for the Startpage into this branch.

---
+++ src/components/DashboardBlockIconTitle.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-flex align-items-center">
+    <DashboardBlockIcon
+      v-if="iconName"
+      :name="iconName"
+      :size="iconSize"
+      customClass="me-2"
+    />
+    <h5 class="m-0"><span>{{ title }}</span></h5>
+    <slot/>
+  </div>
+</template>
+
+<script setup>
+import DashboardBlockIcon from "./DashboardBlockIcon.vue";
+
+defineProps({
+  iconName: {
+    type: String,
+    required: true
+  },
+  iconSize: {
+    type: Number,
+    default: 20,
+    required: false,
+  },
+  title: {
+    type: String,
+    required: true
+  }
+});
+</script>
---
+++ src/components/DashboardTabbedBlock.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="section_block">
+    <div class="section_head">
+      <div class="d-flex align-items-center"><h5 class="m-0"><span>{{ title }}</span></h5></div>
+    </div>
+    <b-tabs content-class="section_block_content" :pills="pills" :card="isCard">
+      <b-tab
+        v-for="(tab, index) in tabList"
+        :key="index"
+        :title="tab.title"
+        :active="index === 0">
+        <div v-html="tab.content"></div>
+      </b-tab>
+    </b-tabs>
+  </div>
+</template>
+
+<style scoped>
+div.section_block div.tabs div ul.nav.nav-pills {
+  margin: 4px;
+  height: 4px;
+  justify-content: center;
+}
+</style>
+
+<script setup>
+// tabList: [{ title: 'Tab 1', content: 'Content 1' }]
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  tabList: {
+    type: Array,
+    required: true,
+  },
+  pills: {
+    type: Boolean,
+    default: false
+  },
+  isCard: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
