a22af0011d231f4651b5a810cc2b04fdb5aaece6
Richard Jansen <demo_epso@proton.me>
2026-06-06 10:56:30 +0200

    show overlay when fetching data

--- src/views/company/startpage/OrderTypesPie.vue
+++ src/views/company/startpage/OrderTypesPie.vue
@@ -5,11 +5,13 @@ import PieChart from "@/components/PieChart.vue"
 // import ChartJsPluginDataLabels from "chartjs-plugin-datalabels";
 
 const chartdataOrderTypesPie = ref({})
+const isLoading = ref(false)
 const orderService = new OrderService()
 const options = {
   responsive: true,
     maintainAspectRatio: false,
     plugins: {
+    // not sure how the plugin works after chartjs upgrade
     datalabels: {
       formatter: (value, _ctx) => {
         return `${value}%`
@@ -33,31 +35,38 @@ function getRandomColorOrderType(txt) {
 }
 
 async function fillPieData() {
-  const orderTypeStatsData = await orderService.getOrderTypesStatsBranch()
-  const thresholdOrderType = .15
-  for (const [orderType, _data] of Object.entries(orderTypeStatsData.order_types)) {
-    if (parseFloat(_data.perc) > thresholdOrderType) {
-      orderTypesLabels.push(orderType)
-      orderTypesColors.push(getRandomColorOrderType(orderType))
-      orderTypesDataPie.push(_data.perc)
-    } else {
-      if (!(orderType in leftOutOrderTypes.data)) {
-        leftOutOrderTypes.data[orderType] = []
+  isLoading.value = true
+  try {
+    const orderTypeStatsData = await orderService.getOrderTypesStatsBranch()
+    const thresholdOrderType = .15
+    for (const [orderType, _data] of Object.entries(orderTypeStatsData.order_types)) {
+      if (parseFloat(_data.perc) > thresholdOrderType) {
+        orderTypesLabels.push(orderType)
+        orderTypesColors.push(getRandomColorOrderType(orderType))
+        orderTypesDataPie.push(_data.perc)
+      } else {
+        if (!(orderType in leftOutOrderTypes.data)) {
+          leftOutOrderTypes.data[orderType] = []
+        }
+        leftOutOrderTypes.count++;
+        leftOutOrderTypes.data[orderType].push({
+          order_type: orderType,
+          data: _data
+        })
       }
-      leftOutOrderTypes.count++;
-      leftOutOrderTypes.data[orderType].push({
-        order_type: orderType,
-        data: _data
-      })
     }
-  }
 
-  chartdataOrderTypesPie.value = {
-    labels: orderTypesLabels,
-    datasets: [{
-      data: orderTypesDataPie,
-      backgroundColor: orderTypesColors,
-    }]
+    chartdataOrderTypesPie.value = {
+      labels: orderTypesLabels,
+      datasets: [{
+        data: orderTypesDataPie,
+        backgroundColor: orderTypesColors,
+      }]
+    }
+    isLoading.value = false
+  } catch (e) {
+    isLoading.value = false
+    console.error('error loading order types status', e)
   }
 }
 
@@ -67,12 +76,14 @@ onMounted(async () => {
 </script>
 
 <template>
-  <pie-chart
-    v-if="chartdataOrderTypesPie"
-    id="pie-chart-order-types"
-    :chart-data="chartdataOrderTypesPie"
-    :options="options"
-  />
+  <b-overlay :show="isLoading" rounded="sm">
+    <pie-chart
+      v-if="chartdataOrderTypesPie"
+      id="pie-chart-order-types"
+      :chart-data="chartdataOrderTypesPie"
+      :options="options"
+    />
+  </b-overlay>
 </template>
 
 <style scoped>
