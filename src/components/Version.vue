62f1744a3bace0d900b9cb68ebfb58f0583e37c9
Richard Jansen <demo_epso@proton.me>
2026-07-30 07:59:33 +0200

    bugfix in fetch version

--- src/components/Version.vue
+++ src/components/Version.vue
@@ -37,7 +37,7 @@ const message = `Using the latest version (${VERSION})`
 
 async function checkVersion() {
   if (document.location.protocol === 'https:') {
-    const data = await axios.get(`https://${document.location.origin}/assets/version.json`).then((response) => response.data)
+    const data = await axios.get(`${document.location.origin}/assets/version.json`).then((response) => response.data)
 
     if (versionToInt(data.version) > versionToInt(this.version)) {
       newVersionAvailable.value = true
