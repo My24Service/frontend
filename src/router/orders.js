54cb13f84851f4c8976dddfbb9e1ac1170f02532
Evert van Brussel <ecvanbrussel@gmail.com>
2026-08-27 00:03:06 +0200

    Add Orders to NavItemsBranch.vue

--- src/router/orders.js
+++ src/router/orders.js
@@ -18,15 +18,15 @@ import {USER_FILTER_TYPE_ORDER} from "@/models/base_user_filter";
 import {createUserFilterRoutes} from "./helpers";
 
 export default [
-// orders
-{
-  component: TheAppLayoutEmpty,
-  path: '/orders',
-  children: [
+  // orders
+  {
+    component: TheAppLayoutEmpty,
+    path: '/orders',
+    children: [
       {
         meta: { needsAuth: false },
         name: 'workorder-view',
-        path: '/orders/orders/workorder/:uuid',
+        path: 'orders/workorder/:uuid',
         components: {
           'app-content': Workorder,
         },
@@ -34,172 +34,179 @@ export default [
           'app-content': route => ({...route.params})
         },
       },
-  ]
-},
-{
-  path: '/orders',
-  component: TheAppLayout,
-  children: [
-    // orders
-    {
-      meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
-      name: 'order-list',
-      path: '/orders/orders',
-      components: {
-        'app-content': OrderList,
-        'app-subnav': SubNavOrders
+    ]
+  },
+  {
+    path: '/orders',
+    component: TheAppLayout,
+    children: [
+      // orders
+      {
+        meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
+        path: 'orders',
+        children: [
+          {
+            name: 'order-list',
+            path: '',
+            components: {
+              'app-content': OrderList,
+              'app-subnav': SubNavOrders
+            },
+          },
+          {
+            path: 'form',
+            children: [
+              {
+                name: 'order-add',
+                path: '',
+                components: {
+                  'app-content': OrderForm,
+                  'app-subnav': SubNavOrders
+                },
+                props: {
+                  'app-content': route => ({...route.params}),
+                  'app-subnav': true
+                },
+              },
+              {
+                name: 'order-edit',
+                path: ':pk(\\d+)',
+                props: {
+                  'app-content': route => ({...route.params}),
+                  'app-subnav': true
+                },
+                components: {
+                  'app-content': OrderForm,
+                  'app-subnav': SubNavOrders
+                },
+              },
+            ],
+     	    },
+          {
+            name: 'order-add-maintenance',
+            path: 'form-maintenance',
+            components: {
+              'app-content': OrderForm,
+              'app-subnav': SubNavOrders
+            },
+            props: {
+              'app-content': {maintenance: true},
+              'app-subnav': true
+            },
+            children: [
+              {
+                name: 'order-add-quotation',
+                path: ':quotation_id',
+                components: {
+                  'app-content': OrderForm,
+                  'app-subnav': SubNavOrders
+                },
+                props: {
+                  'app-content': route => ({...route.params, from_quotation: true}),
+                  'app-subnav': true
+                },
+              },
+            ],
+          },
+          {
+            name: 'order-view',
+            path: 'view/:pk',
+            props: {
+              'app-content': route => ({...route.params}),
+              'app-subnav': true
+            },
+            components: {
+              'app-content': OrderView,
+              'app-subnav': SubNavOrders
+            },
+          },
+          {
+            name: 'order-detail',
+            path: 'detail/:uuid',
+            props: {
+              'app-content': route => ({...route.params}),
+              'app-subnav': true
+            },
+            components: {
+              'app-content': OrderView,
+              'app-subnav': SubNavOrders
+            },
+          },
+        ],
       },
-    },
-    {
-      meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
-      name: 'order-edit',
-      path: '/orders/orders/form/:pk(\\d+)',
-      props: {
-        'app-content': route => ({...route.params}),
-        'app-subnav': true
-      },
-      components: {
-        'app-content': OrderForm,
-        'app-subnav': SubNavOrders
-      },
-    },
-    {
-      meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
-      name: 'order-add',
-      path: '/orders/orders/form',
-      components: {
-        'app-content': OrderForm,
-        'app-subnav': SubNavOrders
-      },
-      props: {
-        'app-content': route => ({...route.params}),
-        'app-subnav': true
-      },
-	  },
-    {
-      name: 'order-add-maintenance',
-      path: '/orders/orders/form-maintenance',
-      components: {
-        'app-content': OrderForm,
-        'app-subnav': SubNavOrders
-      },
-      props: {
-        'app-content': {maintenance: true},
-        'app-subnav': true
-      },
-    },
-    {
-      name: 'order-add-quotation',
-      path: '/orders/orders/form-maintenance/:quotation_id',
-      components: {
-        'app-content': OrderForm,
-        'app-subnav': SubNavOrders
-      },
-      props: {
-        'app-content': route => ({...route.params, from_quotation: true}),
-        'app-subnav': true
-      },
-    },
-    {
-      meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
-      name: 'order-view',
-      path: '/orders/orders/view/:pk',
-      props: {
-        'app-content': route => ({...route.params}),
-        'app-subnav': true
-      },
-      components: {
-        'app-content': OrderView,
-        'app-subnav': SubNavOrders
-      },
-    },
-    {
-      meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
-      name: 'order-detail',
-      path: '/orders/orders/detail/:uuid',
-      props: {
-        'app-content': route => ({...route.params}),
-        'app-subnav': true
-      },
-      components: {
-        'app-content': OrderView,
-        'app-subnav': SubNavOrders
-      },
-    },
-    {
-      meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
-      name: 'orders-not-accepted',
-      path: '/orders/orders-not-accepted',
-      components: {
-        'app-content': OrderList,
-        'app-subnav': SubNavOrders
-      },
-      props: {
-        'app-content': route => ({...route.params, queryMode: 'unaccepted'}),
-        'app-subnav': true
-      }
-    },
-
-    // calendar/schedule
-    {
-      meta: { authLevelNeeded: [
-        AUTH_LEVELS.PLANNING, AUTH_LEVELS.EMPLOYEE, AUTH_LEVELS.CUSTOMER] },
-      name: 'orders-schedule-params',
-      path: '/orders/schedule/:start/:end',
-      components: {
-        'app-content': OrdersSchedule,
-        'app-subnav': SubNavOrders
-      },
-      props: {
-        'app-content': route => ({...route.params}),
-        'app-subnav': true
-      },
-    },
-    {
-      meta: { authLevelNeeded: [
+      // calendar/schedule
+      {
+        meta: { authLevelNeeded: [
           AUTH_LEVELS.PLANNING, AUTH_LEVELS.EMPLOYEE, AUTH_LEVELS.CUSTOMER] },
-      name: 'orders-schedule',
-      path: '/orders/schedule',
-      components: {
-        'app-content': OrdersSchedule,
-        'app-subnav': SubNavOrders
+        name: 'orders-schedule-params',
+        path: 'schedule/:start/:end',
+        components: {
+          'app-content': OrdersSchedule,
+          'app-subnav': SubNavOrders
+        },
+        props: {
+          'app-content': route => ({...route.params}),
+          'app-subnav': true
+        },
       },
-      props: {
-        'app-content': route => ({...route.params}),
-        'app-subnav': true
+      {
+        meta: { authLevelNeeded: [
+            AUTH_LEVELS.PLANNING, AUTH_LEVELS.EMPLOYEE, AUTH_LEVELS.CUSTOMER] },
+        name: 'orders-schedule',
+        path: 'schedule',
+        components: {
+          'app-content': OrdersSchedule,
+          'app-subnav': SubNavOrders
+        },
+        props: {
+          'app-content': route => ({...route.params}),
+          'app-subnav': true
+        },
       },
-    },
 
-    // stats
-    {
-      meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
-      name: 'order-year-stats',
-      path: '/orders/year-stats',
-      components: {
-        'app-content': YearStats,
-        'app-subnav': SubNavOrders
+      // stats
+      {
+        meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
+        name: 'order-year-stats',
+        path: 'year-stats',
+        components: {
+          'app-content': YearStats,
+          'app-subnav': SubNavOrders
+        },
+        props: {
+          'app-content': route => ({...route.params}),
+          'app-subnav': {}
+        },
       },
-      props: {
-        'app-content': route => ({...route.params}),
-        'app-subnav': {}
+      {
+        meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
+        name: 'order-month-stats',
+        path: 'month-stats',
+        components: {
+          'app-content': MonthStats,
+          'app-subnav': SubNavOrders
+        },
+        props: {
+          'app-content': route => ({...route.params}),
+          'app-subnav': {}
+        },
       },
-    },
-    {
-      meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
-      name: 'order-month-stats',
-      path: '/orders/month-stats',
-      components: {
-        'app-content': MonthStats,
-        'app-subnav': SubNavOrders
+      {
+        meta: { authLevelNeeded: [AUTH_LEVELS.CUSTOMER, AUTH_LEVELS.EMPLOYEE] },
+        name: 'orders-not-accepted',
+        path: 'orders-not-accepted',
+        components: {
+          'app-content': OrderList,
+          'app-subnav': SubNavOrders
+        },
+        props: {
+          'app-content': route => ({...route.params, queryMode: 'unaccepted'}),
+          'app-subnav': true
+        }
       },
-      props: {
-        'app-content': route => ({...route.params}),
-        'app-subnav': {}
-      },
-    },
-    // filters
-    ...createUserFilterRoutes('order', 'orders', USER_FILTER_TYPE_ORDER),
+      // filters
+      ...createUserFilterRoutes('order', 'orders', USER_FILTER_TYPE_ORDER),
 
-  ],
-}
+    ],
+  }
 ];
